ca0909be70fb8201f21b1f97bc80a089e986965c
404NOt <2523590493@qq.com>
2022-04-17 22:35:49 +0800

    test

---
+++ Text_4.14/src/TestDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-04-17
+ * Time: 22:23
+ */
+ public class TestDemo {
+     public static void main(String[] args) {
+         System.out.println("lalalala");
+    }
+}
