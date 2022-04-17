0d1a211b36c4c632a1ada7b29c603d98a72d74e7
404NOt <2523590493@qq.com>
2022-04-17 22:21:07 +0800

    44

---
+++ 4_17/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+	public static void main(String[]args){
+		Sysdtem.out,println("hello");
+	}
+}
---
+++ Test2/src/TestDemo1.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-04-17
+ * Time: 18:43
+ */
+ public class TestDemo1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+
+        }
+        System.out.println(args.length);
+        System.out.printf("hhhhh");
+       }
+}
