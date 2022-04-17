0acb51be646f702ba0301839a9180febfc590883
404NOt <2523590493@qq.com>
2022-04-17 18:40:37 +0800

    test2

---
+++ test4_17/src/TestDemo1.java
@@ -0,0 +1,6 @@
+public class TestDemo1 {
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ test4_17/src/TestDemo3.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-04-17
+ * Time: 18:38
+ */
+public class TestDemo3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+    }
+}
