072d85ee89289fd66f4c2617386daaac80eb2f1b
404NOt <2523590493@qq.com>
2022-05-11 17:28:11 +0800

    Test_5_11_3

---
+++ Test5_10_2/src/come/xuk/www/test1.java
@@ -0,0 +1,8 @@
+package come.xuk.www;
+
+public class test1{
+    public static void main(String[] args) {
+        Computer com=new Computer("1","2","3","4");
+        com.Boot();
+    }
+}
--- Test5_10_2/src/test/Test.java
+++ Test5_10_2/src/test/Test.java
@@ -1,7 +1,5 @@
 package test;
 
-import come.xuk.TestDemo2;
-
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -9,6 +7,21 @@ import come.xuk.TestDemo2;
  * Date: 2022-05-11
  * Time: 15:48
  */
+class outerDemo {
+    int t1=11;
+    int t2=12;
+    public static int t3=13;
+    class innerDemo {
+        int t1=22;
+        int t4=24;
+        public static final int t5=25;
+         void sout(innerDemo this) {
+            System.out.println(t1);
+            System.out.println(outerDemo.this.t1);
+            System.out.println(outerDemo.t3);
+        }
+    }
+}
 public class Test {
     public static void main(String[] args) {
 
