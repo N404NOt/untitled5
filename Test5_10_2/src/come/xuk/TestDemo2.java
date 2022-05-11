af2bba682fd03e98ec3a1a45500e11d174f45e38
404NOt <2523590493@qq.com>
2022-05-11 16:02:34 +0800

    Test_5_11_2

--- Test5_10_2/src/come/xuk/TestDemo2.java
+++ Test5_10_2/src/come/xuk/TestDemo2.java
@@ -9,9 +9,12 @@ package come.xuk;
  */
 class washer {
     //这些是字段/成员变量
-    String name;
+    private String name;
+
     int power;
     String fuction;
+    public static String univer="中国";
+
     //这里是功能的实现/方法
     public void washcloth() {
         System.out.println("开始自己洗衣服啦");
@@ -26,12 +29,17 @@ class washer {
 public class TestDemo2 {
     public static void main(String[] args) {
         washer wash=new washer();
-        wash.name="海尔洗衣机";
+        System.out.println(washer.univer);
+        //System.out.println(wash.univer);
+        wash.washcloth();
+        wash.wring();
+        wash.introduction();
+       /* wash.name="海尔洗衣机";
         wash.power=120;
         wash.fuction="洗衣功能与风干功能";
         wash.washcloth();
         wash.wring();
-        wash.introduction();
+        wash.introduction();*/
 
     }
 }
---
+++ Test5_10_2/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import come.xuk.TestDemo2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-05-11
+ * Time: 15:48
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
