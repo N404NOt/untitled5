48038e0682ea11823a9c9e8e1388992a7aa02acb
404NOt <2523590493@qq.com>
2022-05-12 17:27:09 +0800

    Test5_12_1

---
+++ Test5_12/src/Test.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-05-12
+ * Time: 16:48
+ */
+public class Test{
+
+    private float f=1.0f;
+
+    int m=12;
+
+    static int n=1;
+
+    public static void main(String args[]){
+
+        Test t=new Test();
+        t.f=1.0f;
+
+    }
+
+}
