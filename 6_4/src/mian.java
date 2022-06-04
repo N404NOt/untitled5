2d22d36bb970ed5747b5d93b827c4f798638dac0
404NOt <2523590493@qq.com>
2022-06-04 21:32:55 +0800

    111

---
+++ 6_4/src/mian.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-06-04
+ * Time: 21:32
+ */
+public class mian {
+    public static void main(String[] args) {
+
+    }
+}
