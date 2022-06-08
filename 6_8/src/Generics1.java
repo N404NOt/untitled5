f731f387231cc4b8f3b7811b9265c0d7fb6617b1
404NOt <2523590493@qq.com>
2022-06-08 14:18:48 +0800

    Genercis 6_8 13:49 Secound submit

---
+++ 6_8/src/Generics1.java
@@ -0,0 +1,30 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-06-08
+ * Time: 14:08
+ */
+class MyGenerics1 {
+    public Object[] arr=new Object[10];
+
+    public Object getArr(int pos) {
+        return arr[pos];
+    }
+
+    public void setArr(Object arr,int pos) {
+        this.arr[pos] = arr;
+    }
+}
+public class Generics1 {
+    public static void main(String[] args) {
+        MyGenerics1 aa=new MyGenerics1();
+        aa.setArr(1234,0);
+        aa.setArr("aaaaaaa",1);
+        /*for (int i = 0; i < 2; i++) {
+            System.out.println(aa.arr[i]);
+        }*/
+
+    }
+
+}
---
+++ 6_8/src/Test2.java
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-06-08
+ * Time: 13:59
+ */
+class Message<T> {
+    private T message ;
+    public T getMessage() {
+        return message;
+    }
+    public void setMessage(T message) {
+        this.message = message;
+    }
+}
+public class Test2 {
+    public static void main(String[] args) {
+        Message<String> message = new Message() ;
+        message.setMessage("比特就业课欢迎您");
+        fun(message);
+        Message<Integer> message1=new Message<>();
+        message1.setMessage(123);
+        fun(message1);
+    }
+    public static void fun(Message<?> temp){
+        System.out.println(temp.getMessage());
+    }
+}
