6e90cbbf0d51d18c515a91f66e4261bacb59a78b
404NOt <2523590493@qq.com>
2022-06-11 20:34:04 +0800

    6_11 19:42 异常练习 好题！！不会观察思考本质 first

---
+++ 6_11/src/LeetCode/Demo.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-06-11
+ * Time: 19:56
+ */
+class Solution {
+    public String countAndSay(int n) {
+        StringBuilder res = new StringBuilder("1");
+        StringBuilder prev;
+        int count;
+        char key;
+        for (int i = 1; i < n; i++) {
+            prev = res;
+            res = new StringBuilder();
+            count=1;
+            key = prev.charAt(0);
+            for (int j = 1, len = prev.length(); j < len; j++) {
+                if (key != prev.charAt(j)) {
+                    res.append(count).append(key);
+                    count = 1;
+                    key = prev.charAt(j);
+                } else {
+                   count++;
+                }
+            }
+           res.append(count).append(key);
+
+        }
+        return res.toString();
+    }
+}
+public class Demo {
+}
---
+++ 6_11/src/LeetCode/asd.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xuk
+ * Date: 2022-06-11
+ * Time: 19:55
+ */
+class Solution {
+    public String countAndSay(int n) {
+
+    }
+}
