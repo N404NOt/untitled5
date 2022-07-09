9fe749cba8689e8f8cdf49a4573c66c3279e567a
404NOt <2523590493@qq.com>
2022-07-09 21:52:34 +0800

    7_9 Test 21:59

--- 7_9/src/LeetNode/MinStack.java
+++ 7_9/src/LeetNode/MinStack.java
@@ -45,7 +45,7 @@ class MinStack {
         return minStack.peek();
 
     }*/
-}
+
 /*
 class MyStack {
     Deque<Integer> deque;
@@ -81,6 +81,7 @@ class MyStack {
     }
 }
 */
+/*
 class MyStack {
     Queue<Integer> n1;
     Queue<Integer> n2;
@@ -115,5 +116,82 @@ class MyStack {
     public boolean empty() {
         return n2.isEmpty();
 
+    }
+}*/
+public static void main(String[] args) {
+    MyCircularQueue myCircularQueue=new MyCircularQueue(5);
+    myCircularQueue.enQueue(3);
+    myCircularQueue.enQueue(1);
+    myCircularQueue.enQueue(2);
+    myCircularQueue.enQueue(3);
+    myCircularQueue.enQueue(4);
+    System.out.println(myCircularQueue.end);
+    System.out.println(myCircularQueue.Rear());
+    myCircularQueue.deQueue();
+    System.out.println(myCircularQueue.Rear());
+}
+}
+class MyCircularQueue {
+    int pre;
+    int end;
+    int[] arr;
+
+
+    public MyCircularQueue(int k) {
+        arr=new int[k+1];
+        pre=0;
+        end=0;
+
+    }
+
+    public boolean enQueue(int value) {
+        if(isFull()) {
+            return false;
+        }
+        arr[pre]=value;
+        pre=(pre+1)%arr.length;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if(isEmpty()) {
+            return false;
+        }
+        end=(end+1)%arr.length;
+        return true;
+
+
+    }
+
+    public int Front() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return arr[pre-1];
+
+    }
+
+    public int Rear() {
+        if(isEmpty()) {
+            return -1;
+        }
+        int index=(end==0) ? arr.length-1:end-1;
+        return
+
+    }
+
+    public boolean isEmpty() {
+        if(end == pre) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean isFull() {
+        if(end == (pre+1)%arr.length) {
+            return true;
+        }
+        return false;
     }
 }
