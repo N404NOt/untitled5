5d4d27584363fd55b978421ae2bb4dd721ddc5ab
404NOt <2523590493@qq.com>
2022-09-01 23:44:22 +0800

    1

--- 2022_9_1/src/More_Thread.java
+++ 2022_9_1/src/More_Thread.java
@@ -7,17 +7,25 @@
  */
 class Create{
     public static int i = 0;
+    public static Thread[] t = new Thread[20];
     public static void create(int num) {
-        Thread[] t = new Thread[num];
         while (i<num) {
-            t[i] = new Thread(() ->{
-
+            t[i++] = new Thread(() ->{
+                System.out.printf("%d\n" ,i++);
             });
         }
+        i=0;
+
     }
 }
 public class More_Thread {
     public static void main(String[] args) {
+        int num = 20;
+        Create.create(num);
+        for (int i = 0; i < num; i++) {
+            Create.t[i].start();
+        }
+        System.out.println("Hello main");
 
     }
 }
