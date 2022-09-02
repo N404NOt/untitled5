7aaab4b41ae48d81cd9b6be686a15e4ffed79a33
404NOt <2523590493@qq.com>
2022-09-02 09:50:52 +0800

    Thread

--- Tread/src/Demo1.java
+++ Tread/src/Demo1.java
@@ -9,11 +9,11 @@ class MyTread extends Thread {
     public void run() {
         while (true) {
             System.out.println("Hello Thread ");
-            try {
+           /* try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            }
+            }*/
         }
     }
 }
@@ -21,9 +21,14 @@ public class Demo1 {
     public static void main(String[] args) throws InterruptedException {
         MyTread myTread = new MyTread();
         myTread.start();
+        int i=0;
         while ( true) {
             System.out.println("Hello main");
             Thread.sleep(1000);
+            i++;
+            while (i==1) {
+                myTread.interrupt();
+            }
         }
 
     }
